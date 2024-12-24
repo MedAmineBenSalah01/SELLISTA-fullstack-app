442c8ba81bd2ba97a16044a8923303b342cff383
MedAmineBenSalah01 <mohamedaminebensalah6@gmail.com>
2024-12-24 02:18:14 +0100

    fixing gateway and subgraphs

--- server/categories-service/index.js
+++ server/categories-service/index.js
@@ -1,15 +1,50 @@
 const { ApolloServer } = require('@apollo/server');
 const { startStandaloneServer } = require('@apollo/server/standalone');
-const typeDefs = require('./schema');
-const resolvers = require('./resolvers')
+const { gql } = require('graphql-tag');
+const {buildFederatedSchema} = require('@apollo/federation')
+
+
+const categories = [
+  { id: "1", name: "Electronics" },
+  { id: "2", name: "Mobile Phones" },
+];
 
 
 
+const typeDefs = gql`
+type Category @key(fields:"id") {
+  id: ID!
+  name: String!
+}
+extend type Query {
+  categories: [Category!]!
+}
+`;
 
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const resolvers = {
+  Query: {
+    categories: () => categories,
+  },
+  Category: {
+    __resolveReference: (reference) => {
+      return categories.find((c) => c.id === reference.id);
+    },
+  },
+};
+
+
+const server = new ApolloServer({
+  schema : buildFederatedSchema([{typeDefs,resolvers}])
+})
+
+
 startStandaloneServer(server, {
   listen: { port: 4002 },
 }).then(({ url }) => {
   console.log(`Categories service ready at ${url}`);
 });
+
+
+
+
---
+++ server/gateway-service/index.js
@@ -0,0 +1,25 @@
+const { ApolloServer } = require('@apollo/server');
+const { ApolloGateway } = require('@apollo/gateway');
+const { startStandaloneServer } = require('@apollo/server/standalone');
+const { IntrospectAndCompose } = require('@apollo/gateway');
+
+const gateway = new ApolloGateway({
+  supergraphSdl: new IntrospectAndCompose({
+    subgraphs : [
+        { name: 'products', url: 'http://localhost:4001' },
+        { name: 'categories', url: 'http://localhost:4002' },
+
+    ]
+  }),
+});
+
+const server = new ApolloServer({
+  gateway,
+  subscriptions: false,
+});
+
+startStandaloneServer(server, {
+  listen: { port: 4000 }
+}).then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
